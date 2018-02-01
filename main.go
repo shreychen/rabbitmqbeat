20d3e143a644bfb2aefa8a1cbd2b8908096f4093
shreychen <shreychen@vip.qq.com>
2018-02-01 17:36:41 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"os"
 
-	"github.com/elastic/beats/libbeat/logp"
-	"github.com/shreychen/ignitebeat/cmd"
+	"github.com/shreychen/rabbitmqbeat/cmd"
 )
 
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
-		logp.Err(err.Error())
 		os.Exit(1)
 	}
 }
